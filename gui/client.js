296c491a39698c9d2672daf4af98a0feb6752572
Luca <marchisioluca15@gmail.com>
2022-12-03 19:36:49 +0100

    second commit

--- gui/client.js
+++ gui/client.js
@@ -1,25 +1,7 @@
-const input = document.getElementById("open-file")
+const setButton = document.getElementById('btn')
+const titleInput = document.getElementById('title')
 
-const port = 8080
-const baseUrl = "http://localhost:" + port + "/"
-
-input.addEventListener("change", iEvent => {
-    const file = iEvent.target.files[0]
-    const reader = new FileReader()
-    reader.readAsText(file, "UTF-8")
-
-    reader.addEventListener("load", async rEvent => {
-        rEvent.preventDefault()
-        let text = rEvent.target.result
-        if(text == "") return
-        const res = await fetch(baseUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                parcel: text
-            })
-        })
-    })
+setButton.addEventListener('click', () => {
+    const title = titleInput.value
+    window.electronAPI.setTitle(title)
 })
